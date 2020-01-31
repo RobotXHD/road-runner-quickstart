65833ac7efcc0c74232ef490382d7b19cca185e6
RobotX Hunedoara <robotxbp@gmail.com>
2020-01-31 09:35:02 +0200

    Added red paths

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.drive;
 
+import android.net.Uri;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.control.PIDCoefficients;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints;
 import com.qualcomm.hardware.motors.GoBILDA5202Series;
 import com.qualcomm.hardware.motors.NeveRest20Gearmotor;
@@ -88,7 +91,6 @@ public class DriveConstants {
         // note: MotorConfigurationType#getAchieveableMaxTicksPerSecond() isn't quite what we want
         return (MOTOR_CONFIG.getMaxRPM() * MOTOR_CONFIG.getTicksPerRev() / 60.0);
     }
-
     public static double getMotorVelocityF() {
         // see https://docs.google.com/document/d/1tyWrXDfMidwYyP_5H4mZyVgaEswhOC35gvdmP-V-5hA/edit#heading=h.61g9ixenznbx
         return 32767 / getTicksPerSec();
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/rosu_centru.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.teamcode.opModes;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.drive.mecanum.SampleMecanumDriveBase;
+import org.firstinspires.ftc.teamcode.drive.mecanum.SampleMecanumDriveREVOptimized;
+
+@TeleOp
+public class rosu_centru extends LinearOpMode {
+
+    @Override
+
+    public void runOpMode() throws InterruptedException {
+              SampleMecanumDriveBase drive = new SampleMecanumDriveREVOptimized(hardwareMap);
+             Hardware_Scissor_V1 r = new Hardware_Scissor_V1();
+             r.Init(hardwareMap);
+            waitForStart();
+            r.startColect();
+            if (isStopRequested()) return;
+            // pozitie plecare
+            drive.setPoseEstimate(new Pose2d(-35 * 2.54, -62 * 2.54, Math.toRadians(90)));
+            //primul cub
+            drive.followTrajectorySync(
+                    drive.trajectoryBuilder()
+                            .setReversed(false)
+                            .splineTo(new Pose2d(-54 * 2.54, -24*2.54, Math.toRadians(-35)))
+                            .build()
+            );
+            //mers la placa
+            drive.followTrajectorySync(
+                drive.trajectoryBuilder()
+                        .setReversed(true)
+                        .splineTo(new Pose2d(0 * 2.54, -40 * 2.54, Math.toRadians(180)))
+                        .splineTo(new Pose2d(52 * 2.54, -30 * 2.54, Math.toRadians(270)))
+                        .build()
+            );
+            r.startColectReverse();
+            r.prindrePlate();
+
+            sleep(3000);
+            // tras placa in centru
+            drive.followTrajectorySync(
+            drive.trajectoryBuilder()
+                .setReversed(false)
+                .splineTo(new Pose2d(0 * 2.54, -40 * 2.54, Math.toRadians(130)))
+
+                .build()
+            );
+        r.desprindrePlate();
+        r.startColect();
+        sleep(600);
+            //cub 2
+        drive.followTrajectorySync(
+                drive.trajectoryBuilder()
+                        .setReversed(false)
+                        .splineTo(new Pose2d(-31 * 2.54, -24 * 2.54, Math.toRadians(135)))
+                        .build()
+        );
+        //pus placa in colt
+        drive.followTrajectorySync(
+                drive.trajectoryBuilder()
+                        .setReversed(true)
+                        .splineTo(new Pose2d(0 * 2.54, -40 * 2.54, Math.toRadians(-180)))
+                        .splineTo(new Pose2d(50 * 2.54, -50 * 2.54, Math.toRadians(-180)))
+                        .build()
+        );
+        //parcare robot sub bridge
+        drive.followTrajectorySync(
+                drive.trajectoryBuilder()
+                        .setReversed(false)
+                        .splineTo(new Pose2d(0 * 2.54, -40 * 2.54, Math.toRadians(180)))
+
+                        .build()
+        );
+    }
+}
